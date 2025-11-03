7e3235a083e67a80ab612e489fdc1170f62cd0d2
kaiarenja <ge47jow@mytum.de>
2025-11-03 11:41:19 +0100

    added operators list on webiste. added textinput field and output field. added highlighitng of operators in input field and hovering over submit field, including regex for highlighting

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html", title="Bachelorproject")
+
+    # Zweck: Eingaben aus dem Textfeld entgegennehmen und wieder anzeigen.
+    response = None
+    if request.method == "POST":
+        response = request.form.get("user_input", "")
+
+    return render_template("index.html", title="Bachelorproject", response=response)
 
 if __name__ == "__main__":
     app.run(debug=True)
